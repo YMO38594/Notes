db5e0d10e6c4d685429f22b4ffb7dfba80aa0006
YMO38594 <oym38594@gmail.com>
2025-03-21 11:22:28 +0300

    Commit True

--- Notes_App/urls.py
+++ Notes_App/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.signup, name='note_signup'),
     path('notes/', views.note_list, name='note_list'),
     path('login/', LoginView.as_view(template_name='note_login.html'), name='login'),
-    path('create/<int:pk>/', views.note_create, name='note_create'),
+    path('create/', views.note_create, name='note_create'),
     path('update/<int:pk>/', views.note_update, name='note_update'),
     path('delete/<int:pk>/', views.note_delete, name='note_delete'),
 ]
--- Notes_App/views.py
+++ Notes_App/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
 from .forms import NoteForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
     if request.method == 'POST':
-        form = NoteForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
+            print("Form is valid")
             user = form.save()
             login(request, user)
             return redirect('note_list')
     else:
-        form = NoteForm()
+        print("Form is invalid")
+        form = UserCreationForm()
+
     return render(request, 'note_signup_page.html', {'form': form})
 
 @login_required
@@ -21,8 +26,7 @@ def note_list(request):
 
 
 
-def note_create(request, pk):
-    note = get_object_or_404(Note, pk=pk)
+def note_create(request):
     if request.method == 'POST':
         form = NoteForm(request.POST)
         if form.is_valid():
@@ -30,8 +34,8 @@ def note_create(request, pk):
             return redirect('note_list')
     else:
         form = NoteForm()
-    return render(request, 'note_form.html', {'form': form})
 
+    return render(request, 'note_form.html', {'form': form})
 # Update an existing note
 def note_update(request, pk):
     note = get_object_or_404(Note, pk=pk)
